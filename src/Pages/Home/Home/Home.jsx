fe2ab9da17ec997df9f8459d893e38772d53046d
riyad3399 <programminghero.riyad@gmail.com>
2023-05-23 22:45:19 +0600

    added the scroll on animate

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,21 +1,37 @@
+import { useEffect } from "react";
 import setUseTitle from "../../../hooks/setUseTitle";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-    setUseTitle('Home')
-    return (
-        <div>
-            <Banner></Banner>
-            <Category></Category>
-            <PopularMenu></PopularMenu>
-            <Featured></Featured>
-            <Testimonials></Testimonials>
-        </div>
-    );
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  setUseTitle("Home");
+  return (
+    <div>
+      <div data-aos="zoom-in">
+        <Banner></Banner>
+      </div>
+      <div data-aos="fade-left">
+        <Category></Category>
+      </div>
+      <div data-aos="fade-right">
+        <PopularMenu></PopularMenu>
+      </div>
+      <div data-aos="fade-left">
+        <Featured></Featured>
+      </div>
+      <div data-aos="fade-right">
+        <Testimonials></Testimonials>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
